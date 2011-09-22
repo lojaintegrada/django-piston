95fc22df86235de3e744d043267cdcfd1d04460b
stephenm_bex <stephenm@bex.com.au>
2011-09-22 15:06:08 +1000

    Use raw_id_fields for Consumer in site Admin - which prevents Django creating a select box with over a million options!

--- piston/admin.py
+++ piston/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from piston.models import Nonce, Consumer, Token
 
+class ConsumerAdmin(admin.ModelAdmin):
+    raw_id_fields = ('user',)
+
 admin.site.register(Nonce)
-admin.site.register(Consumer)
+admin.site.register(Consumer, ConsumerAdmin)
 admin.site.register(Token)
